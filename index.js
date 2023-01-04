e03ebd6280ba673b82ce8c6530e7fe87ec7872c3
Sylvester <sylvestertrevi@gmail.com>
2023-01-04 03:12:24 -0500

    commit tree 7dd7d4bfc8ef3de8e4551bb7f2d267e5da550da9

---
+++ index.js
@@ -0,0 +1,61 @@
+1 + 80;
+60- 40;
+2 * 3.4;
+5.0 / 2.5;
+
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b
+}
+function multiply(a,b){
+    return a * b
+}
+function divide(a,b){
+    return a / b
+}
+function increment(n){
+    return n += 1
+}
+
+function decrement (n){
+    return n -= 1
+}
+
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n,10)
+}
+/*
+let number = 5;
+number ++;
+number ;
+number --;
+number ;
+
+
+let number = 5;
+number += 3;
+number -= 2;
+number *= 10;
+number /= 5;
+
+
+number = 10;
+function add5(){
+    return (number += 5);
+}
+
+function divideBy3(){
+return (number /= 3);
+}
+
+
+parseInt ("2", 10);
+parseInt ("2.2222", 10);
+parseInt ("nonsense", 10);
+*/
+
